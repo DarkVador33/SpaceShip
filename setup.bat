0b8550271d1ae90ddc63de6465c8a4803c382e89
DarkVador33 <108224457+DarkVador33@users.noreply.github.com>
2022-10-29 17:14:11 +0200

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,11 @@
+@echo off
+title Installing...
+color 1
+echo.
+echo Please wait while installing the python modules...
+echo.
+pip install -r requirements.txt
+echo.
+pause
+cls
+start.bat
---
+++ start.bat
@@ -0,0 +1,11 @@
+@echo off
+color 1
+echo.
+echo It's recommended to use this un fullscreen
+echo.
+pause
+:main
+color 1
+cls
+python main.py
+goto main
